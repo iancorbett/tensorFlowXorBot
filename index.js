de355056da9763d8db1d6a1bfb3a40aad8317979
iancorbett <iancorbett1324@gmail.com>
2025-08-31 22:13:40 -0700

    add outputs to go along with inputs

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 import * as tf from "@tensorflow/tfjs";
 
 // XOR truth table
-const xs = tf.tensor2d([
+const inputs = tf.tensor2d([
     [0, 0],
     [0, 1],
     [1, 0],
     [1, 1],
   ]);
+
+  const outputs = tf.tensor2d([
+    [0],
+    [1],
+    [1],
+    [0],
+  ]);
+  
   
